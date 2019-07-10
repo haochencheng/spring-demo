9f413815ec768151104fb7d100bd579b1b72ece1
haochencheng <haocc@ucommune.com>
2019-07-10 21:15:39 +0800

    添加spring 循环依赖

--- spring-cycleref-demo/src/main/java/config/SpringSingletonBeanConfig.java
+++ spring-cycleref-demo/src/main/java/config/SpringSingletonBeanConfig.java
@@ -2,9 +2,14 @@ package config;
 
 import bean.BeanA;
 import bean.BeanB;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 /**
  * @description:
@@ -12,21 +17,27 @@ import org.springframework.context.annotation.Lazy;
  * @create: 2019-07-10 19:49
  **/
 @Configuration
-public class SpringSingletonBeanConfig {
+public class SpringSingletonBeanConfig implements ApplicationContextAware {
 
     @Bean
     public BeanA beanA(){
         BeanA beanA = new BeanA();
         beanA.setName("beanA");
-        beanA.setBeanB(beanB());
+        beanA.setBeanB(new BeanB());
         return beanA;
     }
 
     @Bean
     public BeanB beanB(){
-        BeanB beanB = new BeanB();
-        beanB.setBeanA(beanA());
-        return beanB;
+        return new BeanB();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        BeanA beanA = applicationContext.getBean(BeanA.class);
+        BeanB beanB = applicationContext.getBean(BeanB.class);
+        beanA.setBeanB(beanB);
+        beanB.setBeanA(beanA);
+
+    }
 }
